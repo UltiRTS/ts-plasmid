e8958e899cdf9aaf9c166eed8dab7d7231804af6
bianyuanop <m13320507359@163.com>
2022-07-19 20:35:21 +0800

    mysql datasource

--- config.ts
+++ config.ts
@@ -1,1 +1,9 @@
 export const selfIP = '127.0.0.1';
+export const dbType = 'mysql';
+export const dbconf = {
+    host: 'localhost',
+    user: 'user',
+    password: 'passsword',
+    database: 'dbname',
+    port: 3306,
+}
