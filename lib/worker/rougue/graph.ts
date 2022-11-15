60e222423dea1d3cddc7bed5cb7c04f189512412
bianyuanop <chen.me.nan@gmail.com>
2022-11-15 14:36:52 -0500

    topological order of nodes order

--- lib/worker/rougue/graph.ts
+++ lib/worker/rougue/graph.ts
@@ -4,10 +4,7 @@ import { Node } from "./node"
 class Graph {
     nodes_count: number
     nodes: Node[] = []
-    probs: {[key: string]: number} = {
-        cache: 0.7,
-        exit: 1
-
+    pp: {[key: string]: number} = {
     }
     exit2boss: number
     adj_list: {
@@ -18,124 +15,51 @@ class Graph {
         combat: number
         decision: number
         store: number
-    }, probs: {
-        cache: number
-        exit: number
-        exit2boss: number
     }) {
         this.nodes_count = nodes_count;
 
-        const prob_sum = probs.cache + probs.exit;
-
-        this.probs = {}
-        this.probs.cache = probs.cache / prob_sum;
-        this.probs.exit = probs.exit / prob_sum + this.probs.cache;
-
-        this.exit2boss = probs.exit2boss;
-
         const partition_sum = partition.combat + partition.decision + partition.store;
-        const pp = {
+        this.pp = {
             combat: 0,
             decision: 0,
             store: 0
         }
-        pp.combat = partition.combat / partition_sum;
-        pp.decision = partition.decision / partition_sum + pp.combat;
-        pp.store = partition.store / partition_sum + pp.decision;
+        this.pp.combat = partition.combat / partition_sum;
+        this.pp.decision = partition.decision / partition_sum + this.pp.combat;
+        this.pp.store = partition.store / partition_sum + this.pp.decision;
+        this.genGraph();
 
-        for(let i=0; i<nodes_count-1; i++) {
+        for(let i=0; i<nodes_count; i++) {
+            const r = Math.random(); 
+            if(r < this.pp.combat) {
+                this.nodes.push(new Node('combat', i));
+            } else if(r >= this.pp.combat && r < this.pp.decision) {
+                this.nodes.push(new Node('combat', i));
+            } else if(r >= this.pp.store){
+                this.nodes.push(new Node('combat', i));
+            }
+        }
+    }
+
+    genGraph() {
+        for(let i=0; i<this.nodes_count; i++) {
             this.adj_list[i] = [];
-            if(i < pp.combat * nodes_count) {
-                this.nodes.push(new Node('combat', i));
-            } else if(i < pp.decision * nodes_count) {
-                this.nodes.push(new Node('decision', i));
-            } else if(i < pp.store) {
-                this.nodes.push(new Node('store', i));
-            } else {
-                this.nodes.push(new Node('combat', i));
+
+            const nodesAvailable = this.nodes_count - i - 1;
+            const nodes4rand = nodesAvailable>=6?6:nodesAvailable;
+            // the last node
+            if(nodes4rand === 0) {
+                break;
+            }
+            const nodes2connect = randomInt(1, nodes4rand+1);
+            for(let j=0; j<nodes2connect; j++) {
+                let adj = randomInt(i+1, this.nodes_count);
+                while(this.adj_list[i].includes(adj)) adj = randomInt(i+1, this.nodes_count);
+                this.adj_list[i].push(adj);
             }
         }
-        this.nodes.push(new Node('exit', nodes_count-1));
-        this.adj_list[nodes_count-1] = [];
-        for(let i=0; i<nodes_count-1; i++) {
-            if(this.outDegree(i) === 0) {
-                console.log('genrating starts from: ', i);
-                this.genGraph(i);
-            }
-        }
+        this.adj_list[this.nodes_count-1] = [];
     }
-
-    genGraph(node: number) {
-        if(node >= this.nodes.length) return;
-        if(node === this.nodes.length-1) return;
-        if(this.outDegree(node) > 0) return;
-
-        switch(this.nodes[node].type) {
-            case 'combat': {
-                let candidates = this.nodes.filter(x => {
-                    return (this.outDegree(x.id) === 0) && (x.id != this.nodes[node].id)
-                }) 
-                // go to exit
-                if(candidates.length === 0) {
-                    this.adj_list[node].push(this.nodes_count-1);
-                    break;
-                }
-                const nextNodesCount = candidates.length>=6?randomInt(6):randomInt(Math.ceil(candidates.length));
-                for(let i=0; i<nextNodesCount; i++) {
-                    const nodeSelected = candidates[randomInt(candidates.length)];
-                    candidates.splice(candidates.indexOf(nodeSelected), 1);
-
-                    this.adj_list[node].push(nodeSelected.id);
-                    this.genGraph(nodeSelected.id);
-                }
-                break;
-            }
-            case 'decision': {
-                let candidates = this.nodes
-                                       .filter(x => {
-                                            return (this.outDegree(x.id) === 0) && (x.id !== this.nodes[node].id)
-                                       }) 
-                // go to exit
-                if(candidates.length === 0) {
-                    this.adj_list[node].push(this.nodes_count-1);
-                    break;
-                }
-
-                const nextNodesCount = candidates.length>=6?randomInt(6):randomInt(Math.ceil(candidates.length));
-                for(let i=0; i<nextNodesCount; i++) {
-                    let nodeSelected = candidates[randomInt(candidates.length)];
-                    candidates.splice(candidates.indexOf(nodeSelected), 1);
-
-                    this.adj_list[node].push(nodeSelected.id);
-                    this.genGraph(nodeSelected.id);
-                }
-                break;
-            }
-            case 'store': {
-                let candidates = this.nodes.filter(x => {
-                    return (this.outDegree(x.id) === 0) && (x.id !== this.nodes[node].id)
-                }) 
-                // go to exit
-                if(candidates.length === 0) {
-                    this.adj_list[node].push(this.nodes_count-1);
-                    break;
-                }
-
-                const nextNodesCount = candidates.length>=6?randomInt(6):randomInt(Math.ceil(candidates.length));
-                for(let i=0; i<nextNodesCount; i++) {
-                    let nodeSelected = candidates[randomInt(candidates.length)];
-                    candidates.splice(candidates.indexOf(nodeSelected), 1);
-
-                    this.adj_list[node].push(nodeSelected.id);
-                    this.genGraph(nodeSelected.id);
-                }
-                break;
-            }
-        }
-
-        if(this.adj_list[node].length === 0) this.adj_list[node].push(this.nodes_count - 1);
-    }
-
 
     outDegree(i: number) {
         if(this.adj_list[i] == null) return 0;
@@ -144,21 +68,15 @@ class Graph {
     }
 
     show() {
-        for(let i=0; i<this.nodes_count; i++) {
-            console.log(this.nodes[i].id, this.nodes[i].type, ':', this.adj_list[i]);
-        }
+        console.log(this.adj_list)
     }
 }
 
 const main = () => {
-    const g = new Graph(20, {
+    const g = new Graph(15, {
         combat: 0.7,
         decision: 0.1,
         store: 0.2
-    }, {
-        cache: 0.95,
-        exit: 0.05,
-        exit2boss: 0.5
     });
 
     g.show();
